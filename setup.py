c8333b8d210ba4a055e8f03fb295fd8be5bc14a4
lucas see <seelcs12@gmail.com>
2023-11-10 16:17:56 -0800

    redo setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
   
-setup( 
-    name='streetviewsampler', 
-    version='0.1', 
-    description='A python package that randomly samples google streetview images from specified locations', 
-    author='pinstripezebra', 
-    author_email='seelcs12@gmail.com', 
-    packages=['StreetViewSampler'], 
-    license="MIT",
-    install_requires=[ 
-        "os", 
-        "pandas",
-        "googlemaps",
-        "random",
-        "requests"
-    ]
-) 
+
+setup(
+    name="streetviewsampler",
+    version="v0.1.1-alpha",
+    author="pinstripezebra",
+    author_email="seelcs12@gmail.com",
+    url="https://github.com/pinstripezebra/StreetView-Library",
+    description="A python package that randomly samples google streetview images from specified locations",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas","googlemaps","requests"],
+)
