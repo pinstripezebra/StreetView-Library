e386d618da36f303dff0c4dae70f96058048d8b7
lucas see <seelcs12@gmail.com>
2023-11-10 16:13:06 -0800

    removing long description

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name='streetviewsampler', 
     version='0.1', 
     description='A python package that randomly samples google streetview images from specified locations', 
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     author='pinstripezebra', 
     author_email='seelcs12@gmail.com', 
     packages=['StreetViewSampler'], 
